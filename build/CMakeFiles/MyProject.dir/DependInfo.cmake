68a24e1fb96cd5b0fcc3044e121aa449dfd8c94a
linlihome <152241068+linlihome@users.noreply.github.com>
2023-11-28 23:32:35 +0800

    Add files via upload

---
+++ build/CMakeFiles/MyProject.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fan/桌面/shell/background.cpp" "CMakeFiles/MyProject.dir/background.cpp.o" "gcc" "CMakeFiles/MyProject.dir/background.cpp.o.d"
+  "/home/fan/桌面/shell/cd.cpp" "CMakeFiles/MyProject.dir/cd.cpp.o" "gcc" "CMakeFiles/MyProject.dir/cd.cpp.o.d"
+  "/home/fan/桌面/shell/cp.cpp" "CMakeFiles/MyProject.dir/cp.cpp.o" "gcc" "CMakeFiles/MyProject.dir/cp.cpp.o.d"
+  "/home/fan/桌面/shell/help.cpp" "CMakeFiles/MyProject.dir/help.cpp.o" "gcc" "CMakeFiles/MyProject.dir/help.cpp.o.d"
+  "/home/fan/桌面/shell/history.cpp" "CMakeFiles/MyProject.dir/history.cpp.o" "gcc" "CMakeFiles/MyProject.dir/history.cpp.o.d"
+  "/home/fan/桌面/shell/ls.cpp" "CMakeFiles/MyProject.dir/ls.cpp.o" "gcc" "CMakeFiles/MyProject.dir/ls.cpp.o.d"
+  "/home/fan/桌面/shell/main.cpp" "CMakeFiles/MyProject.dir/main.cpp.o" "gcc" "CMakeFiles/MyProject.dir/main.cpp.o.d"
+  "/home/fan/桌面/shell/mv.cpp" "CMakeFiles/MyProject.dir/mv.cpp.o" "gcc" "CMakeFiles/MyProject.dir/mv.cpp.o.d"
+  "/home/fan/桌面/shell/pipe.cpp" "CMakeFiles/MyProject.dir/pipe.cpp.o" "gcc" "CMakeFiles/MyProject.dir/pipe.cpp.o.d"
+  "/home/fan/桌面/shell/ps.cpp" "CMakeFiles/MyProject.dir/ps.cpp.o" "gcc" "CMakeFiles/MyProject.dir/ps.cpp.o.d"
+  "/home/fan/桌面/shell/pwd.cpp" "CMakeFiles/MyProject.dir/pwd.cpp.o" "gcc" "CMakeFiles/MyProject.dir/pwd.cpp.o.d"
+  "/home/fan/桌面/shell/redirect.cpp" "CMakeFiles/MyProject.dir/redirect.cpp.o" "gcc" "CMakeFiles/MyProject.dir/redirect.cpp.o.d"
+  "/home/fan/桌面/shell/rm.cpp" "CMakeFiles/MyProject.dir/rm.cpp.o" "gcc" "CMakeFiles/MyProject.dir/rm.cpp.o.d"
+  "/home/fan/桌面/shell/tree.cpp" "CMakeFiles/MyProject.dir/tree.cpp.o" "gcc" "CMakeFiles/MyProject.dir/tree.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/background.cpp.o"
+  "CMakeFiles/MyProject.dir/background.cpp.o.d"
+  "CMakeFiles/MyProject.dir/cd.cpp.o"
+  "CMakeFiles/MyProject.dir/cd.cpp.o.d"
+  "CMakeFiles/MyProject.dir/cp.cpp.o"
+  "CMakeFiles/MyProject.dir/cp.cpp.o.d"
+  "CMakeFiles/MyProject.dir/help.cpp.o"
+  "CMakeFiles/MyProject.dir/help.cpp.o.d"
+  "CMakeFiles/MyProject.dir/history.cpp.o"
+  "CMakeFiles/MyProject.dir/history.cpp.o.d"
+  "CMakeFiles/MyProject.dir/ls.cpp.o"
+  "CMakeFiles/MyProject.dir/ls.cpp.o.d"
+  "CMakeFiles/MyProject.dir/main.cpp.o"
+  "CMakeFiles/MyProject.dir/main.cpp.o.d"
+  "CMakeFiles/MyProject.dir/mv.cpp.o"
+  "CMakeFiles/MyProject.dir/mv.cpp.o.d"
+  "CMakeFiles/MyProject.dir/pipe.cpp.o"
+  "CMakeFiles/MyProject.dir/pipe.cpp.o.d"
+  "CMakeFiles/MyProject.dir/ps.cpp.o"
+  "CMakeFiles/MyProject.dir/ps.cpp.o.d"
+  "CMakeFiles/MyProject.dir/pwd.cpp.o"
+  "CMakeFiles/MyProject.dir/pwd.cpp.o.d"
+  "CMakeFiles/MyProject.dir/redirect.cpp.o"
+  "CMakeFiles/MyProject.dir/redirect.cpp.o.d"
+  "CMakeFiles/MyProject.dir/rm.cpp.o"
+  "CMakeFiles/MyProject.dir/rm.cpp.o.d"
+  "CMakeFiles/MyProject.dir/tree.cpp.o"
+  "CMakeFiles/MyProject.dir/tree.cpp.o.d"
+  "MyProject"
+  "MyProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
